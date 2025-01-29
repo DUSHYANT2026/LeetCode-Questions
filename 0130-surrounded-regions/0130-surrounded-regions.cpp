5b23d29854a4f41c1d77fbea57bb8037c6c6b1a7
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-01-29 14:24:04 +0530

    Time: 5 ms (12.45%), Space: 16.4 MB (14.17%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,38 +1,42 @@
 class Solution {
 private:
-    void dfscheck(int row,int col, int n, int m,vector<vector<int>> &vis,vector<vector<char>> & grid){
+    void dfscheck(int row, int col, int n, int m, vector<vector<int>> &vis, vector<vector<char>> &board) {
         vis[row][col] = 1;
+        
+        vector<int> drow = {-1, 0, 1, 0};
+        vector<int> dcol = {0, 1, 0, -1};
 
-        vector<int> drow = {-1,0,1,0};
-        vector<int> dcol = {0,1,0,-1};
-
-        for(int i=0; i<4; i++){
+        for (int i = 0; i < 4; i++) {
             int nrow = row + drow[i];
             int ncol = col + dcol[i];
-            if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == '0'){
-                dfscheck(nrow,ncol,n,m,vis,grid);
+
+            if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && !vis[nrow][ncol] && board[nrow][ncol] == 'O') {
+                dfscheck(nrow, ncol, n, m, vis, board);
             }
         }
     }
+
 public:
     void solve(vector<vector<char>>& board) {
-        int n = board.size(); int m = board[0].size();
-        if(n == 0) return ;
-        vector<vector<int>> vis(n,vector<int> (m,0));
+        int n = board.size();
+        if (n == 0) return;
+        int m = board[0].size();
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(vis[i][j] == 0 && board[i][j] == 'O' && (i == 0 || j == 0 || i == n-1 || j == m-1)){
-                    dfscheck(i,j,n,m,vis,board);
-                }
-            }
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++) {
+            if (board[i][0] == 'O' && !vis[i][0]) dfscheck(i, 0, n, m, vis, board);
+            if (board[i][m-1] == 'O' && !vis[i][m-1]) dfscheck(i, m-1, n, m, vis, board);
         }
-        
-        for(int i=1; i<n-1; i++){
-            for(int j=1; j<m-1; j++){
-                if(vis[i][j] == 0 && board[i][j] == 'O'){
+        for (int j = 0; j < m; j++) {
+            if (board[0][j] == 'O' && !vis[0][j]) dfscheck(0, j, n, m, vis, board);
+            if (board[n-1][j] == 'O' && !vis[n-1][j]) dfscheck(n-1, j, n, m, vis, board);
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j] && board[i][j] == 'O') {
                     board[i][j] = 'X';
-                     dfscheck(i,j,n,m,vis,board);
                 }
             }
         }
