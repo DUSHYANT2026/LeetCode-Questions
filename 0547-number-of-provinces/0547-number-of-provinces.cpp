d7f5a159766a4eaf28683ad3fc97e50085251cbf
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-04-22 12:14:39 +0530

    0547-NUMBER-OF-PROVINCES Stats: Time: 1 ms (52.3%), Space: 20.3 MB (44.77%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,30 +1,33 @@
 class Solution {
 private:
-    void dfscheck(int node, int n, vector<int> &vis, vector<int> graph[]){
+    void dfscheck(int node,vector<int> &vis, vector<int> graph[]){
         vis[node] = 1;
         for(auto it : graph[node]){
             if(!vis[it]){
-                dfscheck(it,n,vis,graph);
+                dfscheck(it,vis,graph);
             }
         }
     }
 public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int ans = 0;
-        int n = isConnected.size();
-        vector<int> graph[n];
+    int findCircleNum(vector<vector<int>>& isconnected) {
+        int n = isconnected.size();
+        vector<int> graph[n+1];
+
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if(isConnected[i][j] == 1 && i != j){
-                    graph[i].push_back(j);
-                    graph[j].push_back(i);
+                if(isconnected[i][j] == 1){
+                    if(i != j){
+                        graph[i].push_back(j);
+                    }
                 }
             }
         }
         vector<int> vis(n,0);
+        int ans = 0;
         for(int i=0; i<n; i++){
             if(!vis[i]){
-                dfscheck(i,n,vis,graph); ans++;
+                dfscheck(i,vis,graph);
+                ans++;
             }
         }
         return ans;
