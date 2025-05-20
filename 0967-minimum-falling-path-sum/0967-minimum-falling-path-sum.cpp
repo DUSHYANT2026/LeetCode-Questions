c3b848ecfe3f2b1fd8ad5ecf3e67ff2086e4e7aa
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-05-20 18:21:49 +0530

    0967-MINIMUM-FALLING-PATH-SUM Stats: Time: 4 ms (20.07%), Space: 15.1 MB (17.58%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,66 +1,58 @@
 // class Solution {
 // private:
-//     int dpsolve(int i, int j , int n, int m, vector<vector<int>>& dp, vector<vector<int>>& nums){
-//         if(i == n-1) return nums[n-1][j];
+//     int dpcheck(int i, int j, int n, int m, vector<vector<int>> &dp, vector<vector<int>> &grid){
+//         if(i == n-1) return grid[n-1][j];
 
 //         if(dp[i][j] != -1) return dp[i][j];
-//         int count1 = INT_MAX; int count2 = INT_MAX; int count3 = INT_MAX;
+//         int count1 = INT_MAX; int count2 = INT_MAX; int  count3 = INT_MAX;
 //         if(i < n-1){
-//             count1 = nums[i][j] + dpsolve(i+1,j,n,m,dp,nums); 
+//             count1 = grid[i][j] + dpcheck(i+1,j,n,m,dp,grid);
 //         }
 //         if(i < n-1 && j < m-1){
-//             count2 = nums[i][j] + dpsolve(i+1,j+1,n,m,dp,nums);
+//             count2 = grid[i][j] + dpcheck(i+1,j+1,n,m,dp,grid);
 //         }
 //         if(i < n-1 && j > 0){
-//             count3 = nums[i][j] + dpsolve(i+1,j-1,n,m,dp,nums);
+//             count3 = grid[i][j] + dpcheck(i+1,j-1,n,m,dp,grid);
 //         }
-//         return dp[i][j] = min({count1, count2, count3});
+//         return dp[i][j] = min(count1,min(count2,count3));
 //     }
 // public:
-//     int minFallingPathSum(vector<vector<int>>& nums) {
-//         int n = nums.size(); int m = nums[0].size();
-//         vector<vector<int>> dp(n, vector<int> (m,-1));
+//     int minFallingPathSum(vector<vector<int>>& grid) {
+//         int n = grid.size(); int m = grid[0].size();
+//         vector<vector<int>> dp (n, vector<int> (m ,-1));
 //         int ans = INT_MAX;
-//         for(int j=0; j<m; j++){
-//             ans = min(ans, dpsolve(0,j,n,m,dp,nums));
-//         } 
+//         for(int i=0; i<m; i++){
+//             ans = min(ans,dpcheck(0,i,n,m,dp,grid));
+//         }
 //         return ans;
 //     }
 // };
 
-
-
-
-
-
-
-
-
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& nums) {
-        int n = nums.size(); int m = nums[0].size();
-        vector<vector<int>> dp (n,vector<int> (m,-1));
-       
-        for(int j=0; j<m; j++) dp[0][j] = nums[0][j];
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
 
-        for(int i=1; i<n; i++){
-            for(int j=0; j<m; j++){
-                    int down = INT_MAX; int dgleft = INT_MAX; int dgright = INT_MAX;
+        vector<vector<int>> dp(n, vector<int>(m, 0));
 
-                    down = nums[i][j] + dp[i-1][j];
+        for (int j = 0; j < m; ++j) {
+            dp[n-1][j] = grid[n-1][j];
+        }
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = 0; j < m; ++j) {
+                int down = dp[i+1][j];
+                int downLeft = (j > 0) ? dp[i+1][j-1] : INT_MAX;
+                int downRight = (j < m-1) ? dp[i+1][j+1] : INT_MAX;
 
-                    if(j > 0) dgleft = nums[i][j] + dp[i-1][j-1];
-                    else dgleft = INT_MAX;
-            
-                    if(j < m-1) dgright = nums[i][j] + dp[i-1][j+1];
-                    else dgright = INT_MAX;
-
-                    dp[i][j] = min(down,min(dgleft,dgright));
+                dp[i][j] = grid[i][j] + min({down, downLeft, downRight});
             }
         }
         int ans = INT_MAX;
-        for(int j=0; j<m; j++) ans = min(ans,dp[n-1][j]);
+        for (int j = 0; j < m; ++j) {
+            ans = min(ans, dp[0][j]);
+        }
+
         return ans;
     }
 };
