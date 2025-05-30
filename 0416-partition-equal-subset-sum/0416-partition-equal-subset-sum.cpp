108a32d01827092a8e15f0e218862e4546949528
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-05-30 14:29:31 +0530

    0416-PARTITION-EQUAL-SUBSET-SUM Stats: Time: 158 ms (25.88%), Space: 204.2 MB (7.02%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,25 +1,24 @@
 class Solution {
 private:
-    bool dpsolve(int i, int sum, vector<vector<int>> &dp, vector<int> &nums){
+    bool dpcheck(int i, int sum, vector<int> &nums, vector<vector<int>> & dp){
         if(sum == 0) return true;
         if(i == 0) return sum == nums[0];
 
         if(dp[i][sum] != -1) return dp[i][sum];
-        bool notcount = dpsolve(i-1,sum,dp,nums);
-        bool count = false;
-        if(nums[i] <= sum){
-            count = dpsolve(i-1,sum-nums[i],dp,nums);
+        bool count = false; 
+        if(sum >= nums[i]){
+            count = dpcheck(i-1,sum-nums[i],nums,dp);
         }
+        bool notcount = dpcheck(i-1,sum,nums,dp);
         return dp[i][sum] = (count || notcount);
     }
 public:
     bool canPartition(vector<int>& nums) {
-        int n = nums.size(); 
-        int sum = 0; int nsum = 0;
+        int sum  = 0;
         for(auto it : nums) sum += it;
+        int n =nums.size(); 
         if(sum%2 != 0) return false;
-        nsum = sum/2;
-        vector<vector<int>> dp(n,vector<int> (nsum +1 , -1));
-        return dpsolve(n-1,nsum,dp,nums);
+        vector<vector<int>> dp(n, vector<int> (sum+1, -1));
+        return dpcheck(n-1,sum/2,nums,dp);
     }
 };
