e97308142ec6f89230fd797ca46d7fa92e3fabe1
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-06-02 15:25:27 +0530

    0776-N-ARY-TREE-POSTORDER-TRAVERSAL Stats: Time: 17 ms (24.11%), Space: 15.7 MB (45.26%) - LeetHub

--- 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.cpp
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.cpp
@@ -18,27 +18,47 @@ public:
 };
 */
 
+// class Solution {
+// public:
+//     vector<int> postorder(Node* root) {
+//         vector<int> ans;
+//         if(root == NULL) return ans;
+//         stack<Node*> q; 
+//         q.push(root);
+
+//         while(!q.empty()){
+//             int size = q.size();
+//             vector<int> temp;
+//             for(int i=0; i<size; i++){
+//                 auto it = q.top(); q.pop();
+//                 temp.push_back(it->val);
+//                 for(auto i : it->children){
+//                     q.push(i);
+//                 }
+//             }
+//             for(auto it : temp) ans.push_back(it);
+//         }
+//         reverse(ans.begin(),ans.end());
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution {
+private:
+    void dfscheck(Node* root, vector<int> &ans){
+        if(root == NULL) return;
+        for(auto it : root->children){
+            dfscheck(it,ans);
+        }
+        ans.push_back(root->val);
+    }
 public:
     vector<int> postorder(Node* root) {
         vector<int> ans;
-        if(root == NULL) return ans;
-        stack<Node*> q; 
-        q.push(root);
-
-        while(!q.empty()){
-            int size = q.size();
-            vector<int> temp;
-            for(int i=0; i<size; i++){
-                auto it = q.top(); q.pop();
-                temp.push_back(it->val);
-                for(auto i : it->children){
-                    q.push(i);
-                }
-            }
-            for(auto it : temp) ans.push_back(it);
-        }
-        reverse(ans.begin(),ans.end());
+        dfscheck(root, ans);
         return ans;
     }
 };
