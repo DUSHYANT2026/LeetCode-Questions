16a1c4d8d52f5934a829361015d331b8d958146b
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-06-09 10:45:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int findKthNumber(int n, int k) {
+        vector<string> nums;
+        for(int i=1; i<=n; i++) nums.push_back(to_string(i));
+        sort(nums.begin(),nums.end());
+        return stoi(nums[k-1]);
+    }
+};
