97ac4027ea1826c586f861cc5243ef4208ed7212
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-06-09 16:16:28 +0530

    Time: 35 ms (15.84%), Space: 12.7 MB (42.14%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    void dfscheck(int index, int n, vector<int> &temp, vector<int> &nums, set<vector<int>> & ans){
+        if(index == n){
+            ans.insert(temp);
+            return;
+        }
+        for(int i=index; i<n; i++){
+            temp.push_back(nums[i]);
+            swap(nums[index],nums[i]);
+            dfscheck(index+1,n,temp,nums,ans);
+            swap(nums[index],nums[i]);
+            temp.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        set<vector<int>> s;
+        vector<int> temp;
+        dfscheck(0,nums.size(),temp,nums,s);
+        for(auto it : s) ans.push_back(it);
+        return ans;
+    }
+};
