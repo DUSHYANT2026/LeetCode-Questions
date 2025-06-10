292be95d0e3c3dbfcf91c8abe7dbe2188387c6a6
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-06-10 12:02:22 +0530

    Time: 3 ms (34.81%), Space: 9.9 MB (40.32%) - LeetHub

--- 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -3,15 +3,11 @@ public:
     int maxDifference(string s) {
         unordered_map<char,int> mapp;
         for(auto it : s) mapp[it]++;
-
-        int ans = INT_MIN; int even = INT_MAX; int odd = INT_MIN;
+        int odd = INT_MIN; int even = INT_MAX;
         for(auto it : mapp){
-            if(it.second%2 == 0){
-                even = min(even,it.second);
-            }else{
-                odd = max(odd,it.second);
-            }
+            if(it.second%2 == 1) odd = max(odd,it.second);
+            if(it.second%2 == 0) even = min(even,it.second);
         }
-        return odd-even;
+        return odd - even;
     }
 };
