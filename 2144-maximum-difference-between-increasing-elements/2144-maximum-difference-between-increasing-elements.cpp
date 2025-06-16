f7f5a68a23bc2f985bf6590686cb7a849887f0b5
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-06-16 11:45:16 +0530

    2144-MAXIMUM-DIFFERENCE-BETWEEN-INCREASING-ELEMENTS Stats: Time: 0 ms (100%), Space: 12.6 MB (5.67%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
@@ -1,14 +1,27 @@
+// class Solution {
+// public:
+//     int maximumDifference(vector<int>& nums) {
+//         int ans = 0;
+//         for(int i=0; i<nums.size()-1;  i++){
+//             for(int j=i+1; j<nums.size(); j++){
+//                 if(i < j)
+//                 ans = max(ans , (nums[j] - nums[i]));
+//             }
+//         }
+//         return ans == 0 ? -1 : ans;
+//     }
+// };
+
+
 class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
-        int small = INT_MAX; int large = 0; int ans = 0;
-
-        for(int i=0; i<nums.size()-1;  i++){
-            for(int j=i+1; j<nums.size(); j++){
-                if(i < j)
-                ans = max(ans , (nums[j] - nums[i]));
-            }
-        }
+        int n=nums.size(),ans=0; vector<int> pre(n), suff(n);
+        pre[0]=nums[0];
+        suff[n-1]=nums[n-1];
+        for(int i=1;i<n;i++) pre[i]=min(pre[i-1],nums[i]);
+        for(int i=n-2;i>=0;i--) suff[i]=max(suff[i+1],nums[i]);
+        for(int i=0;i<n;i++) ans=max(ans,suff[i]-pre[i]);
         return ans == 0 ? -1 : ans;
     }
 };
