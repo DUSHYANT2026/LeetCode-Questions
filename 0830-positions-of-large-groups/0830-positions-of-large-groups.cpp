a89856968cc6979df8b2c42756e3bc68a57f3722
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-06-22 16:03:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0830-positions-of-large-groups/0830-positions-of-large-groups.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> largeGroupPositions(string s) {
+        vector<vector<int>> ans;
+
+        int start = 0; int end = 0;
+        for(int i=1; i<s.size(); i++){
+            if(s[i-1] == s[i]){
+                end++;
+            }
+            else{
+                if((end - start) >= 2){
+                    ans.push_back({start,end});
+                }
+                start = i;
+                end = i;
+            }
+        }
+        return ans;
+    }
+};
