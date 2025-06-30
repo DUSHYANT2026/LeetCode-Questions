d5c5217c8bc71edff9a877c1c73ec558addb8102
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-06-30 19:15:07 +0530

    Time: 0 ms (100%), Space: 15.2 MB (66.13%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,8 +1,40 @@
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         unordered_map<int,int> mapp; for(int i=0; i<nums.size(); i++) mapp[nums[i]] = i;
+//         if(mapp.find(target) != mapp.end()) return mapp[target];
+//         return -1;
+//     }
+// };
+
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        unordered_map<int,int> mapp; for(int i=0; i<nums.size(); i++) mapp[nums[i]] = i;
-        if(mapp.find(target) != mapp.end()) return mapp[target];
+        int n = nums.size();
+        int lo = 0;
+        int hi = n-1;
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(nums[mid]==target)
+            return mid;
+            if(nums[lo]<=nums[mid]){
+                if(nums[lo]<=target && target<= nums[mid]){
+                    hi = mid-1;
+                }
+                else{
+                    lo = mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[hi]){
+                    lo = mid+1;
+                }
+                else{
+                    hi = mid-1;
+                }
+            }
+        }
         return -1;
     }
 };
