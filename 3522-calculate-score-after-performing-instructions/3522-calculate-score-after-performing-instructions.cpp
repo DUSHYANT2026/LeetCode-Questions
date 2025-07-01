c56e258a67841156abea9c6f097d740792b23879
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-07-01 11:46:37 +0530

    Time: 4 ms (63.58%), Space: 169.5 MB (51.24%) - LeetHub

---
+++ 3522-calculate-score-after-performing-instructions/3522-calculate-score-after-performing-instructions.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long calculateScore(vector<string>& nums, vector<int>& values) {
+        int n = nums.size(); long long ans = 0;
+        vector<int> vis(n,0);
+
+        for(int i=0; i<nums.size(); i++){
+            if(vis[i] == 0){
+                vis[i] = 1;
+                i = i%n;
+                if(nums[i] == "jump"){
+                    i = (i + values[i]-1);
+                }else if(nums[i] == "add"){
+                    ans += values[i];
+                }
+            }
+            else return ans;
+        }
+        return ans;
+    }
+};
