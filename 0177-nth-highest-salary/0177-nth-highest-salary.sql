b858261480b41013526ce616254c08b37974264c
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-07-02 19:48:56 +0530

    Time: 433 ms (64.16%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,18 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  SET N = N-1;
+  RETURN (
+    SELECT DISTINCT salary from Employee order by salary desc limit 1 offset N
+  );
+END
+
+
+
+-- CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+-- BEGIN
+--   DECLARE ans INT;
+--   SET N = N-1;
+--   SELECT DISTINCT salary INTO ans FROM Employee ORDER BY salary DESC 
+--   LIMIT 1 OFFSET N;
+--   RETURN ans;
+-- END
