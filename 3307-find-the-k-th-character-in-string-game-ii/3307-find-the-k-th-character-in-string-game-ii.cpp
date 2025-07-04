794c914d459482678daeceba051a0f30cafda676
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-07-04 22:15:31 +0530

    Time: 0 ms (100%), Space: 28.3 MB (73.42%) - LeetHub

--- 3307-find-the-k-th-character-in-string-game-ii/3307-find-the-k-th-character-in-string-game-ii.cpp
+++ 3307-find-the-k-th-character-in-string-game-ii/3307-find-the-k-th-character-in-string-game-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    vector<long long >pow;
-    int solve(int ind,long long k,vector<int>&operations){
-        if(ind<0)
-            return 0;
-        if(ind>=55)
-            return solve(ind-1,k,operations);
-        if(pow[ind]<k)
-            return operations[ind]+solve(ind-1,k-pow[ind],operations);
-        return solve(ind-1,k,operations);
-    }
+
     char kthCharacter(long long k, vector<int>& operations) {
-        pow.resize(55,1);
-        for(int i=1;i<55;i++){
-            pow[i]=pow[i-1]*2;
+        if(k==1) return 'a';
+        long long len=1;
+        long long newk=-1;
+        int op=-1;
+        for(int i=0; i<operations.size(); i++){
+            len *= 2;
+            if(len>=k){
+                op= operations[i];
+                newk= k-len/2;
+                break;
+            }
         }
-        int n=operations.size();
-        int ans=solve(n-1,k,operations);
-        return 'a'+ans%26;
+        char ch= kthCharacter(newk, operations);
+        if(op==0){
+            return ch;
+        }
+        return (ch=='z')?'a':ch+1;
     }
 };
