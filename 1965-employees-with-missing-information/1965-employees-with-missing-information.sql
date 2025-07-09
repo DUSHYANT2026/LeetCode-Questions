9b146a6385be9762e21a01715eea70b4dc73c34e
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-07-09 22:14:41 +0530

    Time: 542 ms (72.48%), Space: 0B (100%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,4 +1,18 @@
 # Write your MySQL query statement below
-with temp as( select employee_id from employees union all select employee_id from salaries)
+-- with temp as( select employee_id from employees union all select employee_id from salaries)
 
-select employee_id from temp where employee_id not in (select e.employee_id from employees e join salaries s on e.employee_id = s.employee_id) order by employee_id
+-- select employee_id from temp where employee_id not in (select e.employee_id from employees e join salaries s on e.employee_id = s.employee_id) order by employee_id
+
+
+(SELECT E.employee_id
+FROM Employees E
+LEFT JOIN Salaries S on E.employee_id = S.employee_id
+WHERE S.salary IS NULL
+
+UNION
+
+SELECT S.employee_id
+FROM Employees E
+RIGHT JOIN Salaries S on E.employee_id = S.employee_id
+WHERE E.name IS NULL)
+ORDER BY employee_id
