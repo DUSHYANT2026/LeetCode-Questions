bc02a8386cd20806dce5c06a3663c5fe3275026c
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-08-01 23:41:57 +0530

    0018-4SUM Stats: Time: 202 ms (9.74%), Space: 53.5 MB (12.2%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -6,11 +6,11 @@ public:
         int n = nums.size();
 
         set<vector<int>> s;
-        for(int i=0; i<n-3; i++){
-            for(int j=i+1; j<n-2; j++){
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
                 int k = j+1; int l = n-1;
                 while(k < l){
-                    long long int sum = nums[i] + nums[j] + nums[k] + nums[l];
+                    long long int sum = (long long)nums[i] + (long long)nums[j] + (long long)nums[k] + (long long)nums[l];
                     if(sum == target){
                         s.insert({nums[i] , nums[j] , nums[k] , nums[l]});
                         k++; l--;
