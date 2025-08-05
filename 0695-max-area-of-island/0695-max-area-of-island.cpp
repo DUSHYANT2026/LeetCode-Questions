939a19da8c20be1cf455306a8b56eb469332798d
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-08-05 17:17:22 +0530

    Time: 11 ms (15.9%), Space: 30.9 MB (26.24%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,29 +1,31 @@
 class Solution {
 private:
-    void dfscheck(int& count,int row, int col, int n, int m, vector<vector<int>> &vis, vector<vector<int>> &grid){
+    void dfscheck(int row, int col, int n, int m,  vector<vector<int>> &vis, vector<vector<int>>&grid,
+    int &sum ){
         vis[row][col] = 1;
-        vector<int> drow = {1,0,-1,0};
-        vector<int> dcol = {0,1,0,-1};
+        sum++;
+        vector<pair<int,int>> del = {{-1,0},{0,1},{1,0},{0,-1}};
+
         for(int i=0; i<4; i++){
-            int nrow = row + drow[i];
-            int ncol = col + dcol[i];
-            if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && vis[nrow][ncol] != 1 && grid[nrow][ncol] == 1){
-                count++;
-                dfscheck(count,nrow,ncol,n,m,vis,grid);
+            int nrow = row + del[i].first;
+            int ncol = col + del[i].second;
+
+            if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && grid[nrow][ncol]!=0 && vis[nrow][ncol] == 0){
+                dfscheck(nrow,ncol,n,m,vis,grid,sum);
             }
         }
     }
 public:
-    int maxAreaOfIsland(vector<vector<int>>& board) {
-        int n = board.size(); int m  = board[0].size();
-        vector<vector<int>> vis(n , vector<int> (m,0));
-        int ans = 0; int count = 1;
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int n =  grid.size(); int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int> (m,0));
+        int ans = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(!vis[i][j] && board[i][j] == 1){
-                    dfscheck(count,i,j,n,m,vis,board);
-                    ans = max(ans,count);
-                    count = 1;
+                if(grid[i][j] != 0 && vis[i][j] == 0){
+                    int sum = 0;
+                    dfscheck(i,j,n,m,vis,grid,sum);
+                    ans = max(ans,sum);
                 }
             }
         }
