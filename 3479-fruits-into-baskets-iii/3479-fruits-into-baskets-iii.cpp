6a2564a380b58b4fc10916b8be853b50553e28b2
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-08-06 10:39:12 +0530

    Time: 530 ms (26.74%), Space: 181.1 MB (89.41%) - LeetHub

--- 3479-fruits-into-baskets-iii/3479-fruits-into-baskets-iii.cpp
+++ 3479-fruits-into-baskets-iii/3479-fruits-into-baskets-iii.cpp
@@ -1,18 +1,89 @@
+// class Solution {
+// public:
+//     int numOfUnplacedFruits(vector<int>& nums1, vector<int>& nums2) {
+//         vector<int> temp = nums2;
+//         int count = 0;
+//         for(int i=0; i<nums1.size(); i++){
+//             int mark = -1;
+//             for(int j=0; j<nums2.size(); j++){
+//                 if(nums1[i] <= nums2[j]){
+//                     mark = j; break;
+//                 }
+//             }
+//             cout<<mark<<endl;
+//             if(mark != -1) nums2.erase(nums2.begin()+mark);
+//         }
+//         return nums2.size();
+//     }
+// };
+
 class Solution {
 public:
-    int numOfUnplacedFruits(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> temp = nums2;
+    int segTree[400007];
+    vector<int> baskets;
+
+    void build(int p, int l, int r) {
+        if (l == r) {
+            segTree[p] = baskets[l];
+            return;
+        }
+        int mid = (l + r) >> 1;
+        build(p << 1, l, mid);
+        build(p << 1 | 1, mid + 1, r);
+        segTree[p] = max(segTree[p << 1], segTree[p << 1 | 1]);
+    }
+
+    int query(int p, int l, int r, int ql, int qr) {
+        if (ql > r || qr < l) {
+            return INT_MIN;
+        }
+        if (ql <= l && r <= qr) {
+            return segTree[p];
+        }
+        int mid = (l + r) >> 1;
+        return max(query(p << 1, l, mid, ql, qr),
+                   query(p << 1 | 1, mid + 1, r, ql, qr));
+    }
+
+    void update(int p, int l, int r, int pos, int val) {
+        if (l == r) {
+            segTree[p] = val;
+            return;
+        }
+        int mid = (l + r) >> 1;
+        if (pos <= mid) {
+            update(p << 1, l, mid, pos, val);
+        } else {
+            update(p << 1 | 1, mid + 1, r, pos, val);
+        }
+        segTree[p] = max(segTree[p << 1], segTree[p << 1 | 1]);
+    }
+
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        this->baskets = baskets;
+        int m = baskets.size();
         int count = 0;
-        for(int i=0; i<nums1.size(); i++){
-            int mark = -1;
-            for(int j=0; j<nums2.size(); j++){
-                if(nums1[i] <= nums2[j]){
-                    mark = j; break;
+        if (m == 0) {
+            return fruits.size();
+        }
+        build(1, 0, m - 1);
+        for (int i = 0; i < m; i++) {
+            int l = 0, r = m - 1, res = -1;
+            while (l <= r) {
+                int mid = (l + r) >> 1;
+                if (query(1, 0, m - 1, 0, mid) >= fruits[i]) {
+                    res = mid;
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
                 }
             }
-            cout<<mark<<endl;
-            if(mark != -1) nums2.erase(nums2.begin()+mark);
+            if (res != -1 && baskets[res] >= fruits[i]) {
+                update(1, 0, m - 1, res, INT_MIN);
+            } else {
+                count++;
+            }
         }
-        return nums2.size();
+        return count;
     }
 };
