7f342499065ebb03e4100c7ec12190e9554d00b7
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-08-15 19:27:19 +0530

    Time: 2 ms (85.57%), Space: 11.5 MB (64.22%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,19 +1,57 @@
+// class Solution {
+// public:
+//     vector<int> findAnagrams(string s, string p) {
+//         if(p.size() > s.size()) return {};
+
+//         int size = p.size();
+//         sort(p.begin(),p.end());
+//         vector<int> ans;
+
+//         for(int i=0; i<s.size()-size+1; i++){
+//             string temp = s.substr(i,size);
+//             sort(temp.begin(),temp.end());
+//             if(temp == p){
+//                 ans.push_back(i);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        if(p.size() > s.size()) return {};
-
-        int size = p.size();
-        sort(p.begin(),p.end());
-        vector<int> ans;
-
-        for(int i=0; i<s.size()-size+1; i++){
-            string temp = s.substr(i,size);
-            sort(temp.begin(),temp.end());
-            if(temp == p){
-                ans.push_back(i);
+        if (p.size() > s.size()) return {};
+        
+        vector<int> result;
+        vector<int> pCount(26, 0);
+        vector<int> windowCount(26, 0);
+        
+        for (char c : p) {
+            pCount[c - 'a']++;
+        }
+        
+        int windowSize = p.size();
+        
+        for (int i = 0; i < windowSize; i++) {
+            windowCount[s[i] - 'a']++;
+        }
+        
+        if (pCount == windowCount) {
+            result.push_back(0);
+        }
+        
+        for (int i = windowSize; i < s.size(); i++) {
+            windowCount[s[i] - 'a']++;
+            windowCount[s[i - windowSize] - 'a']--;
+            
+            if (pCount == windowCount) {
+                result.push_back(i - windowSize + 1);
             }
         }
-        return ans;
+        
+        return result;
     }
 };
