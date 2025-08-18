b0d54b4f05a6ea7f1ad304b8032d5025513b3990
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-08-18 16:13:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string longestWord(vector<string>& words) {
+
+        unordered_map<string,int> mapp;
+        for(auto it : words) mapp[it]++;
+
+        vector<pair<int,string>> nums;
+        for(int i=0; i<words.size(); i++){
+            string temp = words[i];
+            int count = 0;
+            string ntemp = "";
+            for(int j=0; j<temp.size(); j++){
+                ntemp += temp[j];
+                if(mapp.find(ntemp) != mapp.end()){
+                    count++;
+                }
+                else {
+                    nums.push_back({count,temp});
+                    break;
+                }
+            }
+            if(count != 0){
+                nums.push_back({count,words[i]});
+            }
+        } 
+        auto coustom = [](pair<int,string> & a, pair<int,string> &b){
+            if(a.first == b.first){
+                return a.second < b.second;
+            }
+            return a.first > b.first;
+        };
+        sort(nums.begin(),nums.end(), coustom);
+        for(auto it : nums){
+            cout<<it.first<<" "<<it.second<<endl;
+        }
+        return (nums.first == 0) ? "" : nums[0].second;
+    }
+};
