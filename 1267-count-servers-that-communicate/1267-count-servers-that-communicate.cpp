e2d1b2d56af0101ef8c243107dc463499bcdd964
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-09-01 18:59:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void dfscheck(int row, int col, int n, int m,long long& sum, vector<vector<int>> &vis, vector<vector<int>> &grid){
+        vis[row][col] = 1; 
+        sum += grid[row][col];
+
+        vector<pair<int,int>> del = {{1,0},{-1,0},{0,1},{0,-1}};
+        for(int i=0; i<4; i++){
+            int nrow = row + del[i].first;
+            int ncol = col + del[i].second;
+            if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && !vis[nrow][ncol] && grid[nrow][ncol] != 0){
+                dfscheck(nrow,ncol,n,m,sum,vis,grid);
+            }
+        }
+    }
+    int countServers(vector<vector<int>>& grid) {
+        int n = grid.size(); int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int> (m, 0));
+
+        int count = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                long long sum = 0;
+                if(!vis[i][j] && grid[i][j] != 0){
+                    dfscheck(i,j,n,m,sum,vis,grid);
+                    cout<<sum<<endl;
+                    if(sum != 1) count += sum;
+                }
+            }
+        }
+        return count;
+    }
+};
