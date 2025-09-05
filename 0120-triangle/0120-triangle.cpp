8f521edb180a2329a74492265bce771e6bb7a27a
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-09-06 02:18:51 +0530

    Time: 3 ms (14.19%), Space: 13.5 MB (18.47%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,19 +1,20 @@
 class Solution {
 private:
-    int dpsolve(int i, int j,int n, vector<vector<int>>& dp, vector<vector<int>>& nums){
+    int dpcheck(int i, int j,int n, vector<vector<int>> &nums, vector<vector<int>>&dp){
+
         if(i == n-1) return nums[n-1][j];
-        if(dp[i][j] != -1) return dp[i][j];
 
-        int count1 = INT_MAX; int count2 = INT_MAX;
-        if(j < n-1) count1 = nums[i][j] + dpsolve(i+1,j,n,dp,nums);
-        if(j < n-1) count2 = nums[i][j] + dpsolve(i+1,j+1,n,dp,nums);
+        if(dp[i][j] != -2) return dp[i][j];
 
-        return dp[i][j] = min(count1,count2);
+        int down = nums[i][j] + dpcheck(i+1,j,n,nums,dp);
+        int left = nums[i][j] + dpcheck(i+1,j+1,n,nums,dp);
+
+        return dp[i][j] = min(down,left);  
     }
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        vector<vector<int>> dp(n+1, vector<int> (n+1,-1));
-        return dpsolve(0,0,n,dp,triangle);
+    int minimumTotal(vector<vector<int>>& nums) {
+        int n = nums.size(); 
+        vector<vector<int>> dp(n,vector<int> (n,-2));
+        return dpcheck(0,0,n,nums,dp);
     }
 };
