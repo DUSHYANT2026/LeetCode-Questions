d38c3678c124a8d0f0a6fd7ff17de2af6c02dafe
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-10-16 23:03:48 +0530

    Time: 3 ms (51.63%), Space: 16.5 MB (51.96%) - LeetHub

---
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) {
+        unordered_map<string,int> mp;
+        vector<string> ans;
+        for(int i=0;i<words.size();i++){
+            string x=words[i];
+            sort(x.begin(),x.end());
+            if(mp[x] == 0){
+                if(mp.size()!=0){
+                    mp.clear();
+                }
+                ans.push_back(words[i]);
+                mp[x]++;
+            } 
+            // else if(mp[x]==1) continue;        
+        }
+        return ans;
+    }
+};
+
+
