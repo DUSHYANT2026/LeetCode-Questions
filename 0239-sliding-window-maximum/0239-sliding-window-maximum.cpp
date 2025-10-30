ee3867dc942b6a480eec2697b662620bbc1e71b5
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-10-30 15:24:00 +0530

    Time: 63 ms (19.16%), Space: 153.2 MB (17.73%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,41 @@
+// class Solution {
+// public:
+//     int check(vector<int> nums){
+//         sort(nums.begin(),nums.end());
+//         return nums[nums.size()-1];
+//     }
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         vector<int> ans;
+//         vector<int> temp;
+//         for(int i=0; i<k; i++){
+//             temp.push_back(nums[i]);
+//         }
+//         ans.push_back(check(temp));
+
+//         for(int i=k; i<nums.size(); i++){
+//             temp.erase(temp.begin());
+//             temp.push_back(nums[i]);
+//             ans.push_back(check(temp));
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int,int>> pq;
+        vector<int> ans;
+
+        for(int i=0; i<nums.size(); i++){
+            pq.push({nums[i],i});
+            if(i+1 >= k){
+                while(pq.top().second < (i-k+1)) pq.pop();
+                ans.push_back(pq.top().first);
+            }
+        }
+        return ans;
+    }
+};
