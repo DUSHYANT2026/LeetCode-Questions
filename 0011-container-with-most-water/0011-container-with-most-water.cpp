1e9b0b12c8b0526ce1120c514df94795ca59f526
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-11-05 21:45:18 +0530

    Time: 3 ms (36.07%), Space: 62.9 MB (48.18%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int maxArea(vector<int>& nums) {
-       int ans = INT_MIN;
-       int i=0; int j = nums.size()-1;
+        int ans = INT_MIN;
+        int i = 0; int j = nums.size()-1;
 
-       while(i < j){
-        if(nums[i] <= nums[j]){
-            ans = max(ans, (nums[i]*(j-i)));
-            i++; 
-        }else{
-            ans = max(ans, (nums[j]*(j-i)));
-            j--;
+        while(i < j){
+            if(nums[i] < nums[j]){
+                ans = max(ans, nums[i] * abs(i-j));
+                i++;
+            }
+            else {
+                ans = max(ans, nums[j] * abs(i-j));
+                j--;
+            }
         }
-       } 
-       return ans;
+        return ans;
     }
 };
