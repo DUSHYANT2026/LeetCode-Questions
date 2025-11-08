8382be40845b624b5ef31200c51740f2da7660d7
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-11-09 02:47:58 +0530

    Time: 8 ms (26.46%), Space: 99.9 MB (61.45%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -3,16 +3,11 @@ public:
     int minCost(string nums, vector<int>& time) {
         
         int ans = 0;
-        int index = INT_MAX;
-        int newindex = INT_MIN;
+        int index = INT_MAX; int newindex = INT_MIN;
         for(int i=1; i<nums.size(); i++){
             if(nums[i-1] == nums[i]){
-                cout<<time[i-1]<<" "<<time[i]<<endl;
-
-                index = min(time[i-1],time[i]);
-                newindex = max(time[i-1],time[i]);
-                ans += index;
-                time[i] = newindex;
+                ans += min(time[i-1],time[i]);
+                time[i] = max(time[i-1],time[i]);
             }
         }
         return ans;
