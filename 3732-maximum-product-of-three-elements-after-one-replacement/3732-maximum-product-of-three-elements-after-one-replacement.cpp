976f37e3d06657afb95f513a27416ae04e63e23d
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-11-12 20:39:49 +0530

    Time: 30 ms (76.61%), Space: 124.2 MB (95.5%) - LeetHub

--- 3732-maximum-product-of-three-elements-after-one-replacement/3732-maximum-product-of-three-elements-after-one-replacement.cpp
+++ 3732-maximum-product-of-three-elements-after-one-replacement/3732-maximum-product-of-three-elements-after-one-replacement.cpp
@@ -1,22 +1,29 @@
+// class Solution {
+// public:
+//     long long maxProduct(vector<int>& nums) {
+//         vector<pair<long long,bool>> temp;
+
+//         for(auto it : nums){
+//             if(it >= 0) temp.push_back({it,true});
+//             else temp.push_back({abs(it),false});
+//         }
+//         sort(temp.begin(),temp.end());
+//         int n = nums.size();
+//         bool mark1 = temp[n-1].second;
+//         bool mark2 = temp[n-2].second;
+//         long long num1 = temp[n-1].first;
+//         long long num2 = temp[n-2].first;
+//         return num1*num2*1e5;
+//     }
+// };
+
+
+
 class Solution {
 public:
     long long maxProduct(vector<int>& nums) {
-        vector<pair<long long,bool>> temp;
-
-        for(auto it : nums){
-            if(it >= 0) temp.push_back({it,true});
-            else temp.push_back({abs(it),false});
-        }
-        sort(temp.begin(),temp.end());
-        // for(auto it : temp){
-        //     cout<<it.first<<" "<<it.second;
-        //     cout<<endl;
-        // }
-        int n = nums.size();
-        bool mark1 = temp[n-1].second;
-        bool mark2 = temp[n-2].second;
-        long long num1 = temp[n-1].first;
-        long long num2 = temp[n-2].first;
-        return num1*num2*1e5;
+        for(int i=0; i<nums.size(); i++) nums[i] = abs(nums[i]);
+        sort(nums.begin(),nums.end());
+        return (long long)nums[nums.size()-1] * (long long)nums[nums.size()-2] * 1e5;
     }
 };
