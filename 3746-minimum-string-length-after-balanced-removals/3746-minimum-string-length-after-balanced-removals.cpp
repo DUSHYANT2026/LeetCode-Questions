4ac935e287158dfa334c9b11b407df3b7f049fdf
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-11-16 13:46:14 +0530

    Time: 0 ms (100%), Space: 14.7 MB (33.33%) - LeetHub

---
+++ 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int minLengthAfterRemovals(string s) {
+        int count1 = 0; int count2 = 0;
+        for(auto it : s){ if(it == 'a') count1++; else count2++; }
+        return abs(count1-count2);
+    }
+};
