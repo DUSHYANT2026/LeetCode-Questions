0298996d32870dd37edc16bd157bb087252cb4bd
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-12-08 11:44:04 +0530

    Time: 9 ms (46.15%), Space: 34.5 MB (30.77%) - LeetHub

--- 3769-sort-integers-by-binary-reflection/3769-sort-integers-by-binary-reflection.cpp
+++ 3769-sort-integers-by-binary-reflection/3769-sort-integers-by-binary-reflection.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    string converttobinary(int n){
-        string ans = "";
+    int check(int n){
+        int ans = 0;
+        string s = "";
         while(n != 0){
-            ans += '0' + n%2; 
+            s += '0' + n%2; 
             n = n/2;
         }
-        return ans;
-    }
-    int converttonumber(string s){
-        int ans = 0;
         reverse(s.begin(),s.end());
         for(int i=0; i<s.size(); i++){
             if(s[i] == '1') ans += pow(2,i);
@@ -18,11 +15,8 @@ public:
     }
     vector<int> sortByReflection(vector<int>& nums) {
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-
         for(int i=0; i<nums.size(); i++){
-            string temp1 = converttobinary(nums[i]);
-            int check = converttonumber(temp1);
-            pq.push({check,nums[i]});
+            pq.push({check(nums[i]),nums[i]});
         }
         vector<int> ans;
         while(!pq.empty()){
