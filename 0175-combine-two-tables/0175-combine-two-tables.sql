a302db88f2469483bb7e66689f072b0036c85749
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-12-19 22:04:52 +0530

    Time: 376 ms (70.01%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,2 +1,13 @@
 # Write your MySQL query statement below
-select lastName, FirstName, city, state from person p left join address a on p.personId = a.personId 
+
+select lastName, FirstName, city, state from person p  left join address a on p.personId = a.personId 
+
+
+
+-- with temp as(
+
+-- select personId from person
+-- union all
+-- select personId from address
+-- )
+-- select distinct(personId) from temp order by personId desc
