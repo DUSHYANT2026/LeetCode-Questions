1f35bc3a67fee263f8ca552b183b3836b1e16706
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2025-12-24 22:28:38 +0530

    0547-NUMBER-OF-PROVINCES Stats: Time: 3 ms (40.93%), Space: 21 MB (21.5%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,6 +1,31 @@
-class Solution { public:
-    void dfscheck(int node, vector<int> &vis, vector<int> graph[]){ vis[node] = 1; for(auto it : graph[node]){if(!vis[it]){dfscheck(it,vis,graph);}}}
+class Solution {
+public:
+    void dfscheck(int node, vector<int> &vis, vector<int> graph[]){
+        vis[node] = 1;
+        for(auto it : graph[node]){
+            if(!vis[it]){
+                dfscheck(it,vis,graph);
+            }
+        }
+    }
     int findCircleNum(vector<vector<int>>& nums) {
-        int n = nums.size(); vector<int> graph[n]; int ans = 0; vector<int> vis(n,0);
-        for(int i=0; i<n; i++){ for(int j=0; j<n; j++) {if(nums[i][j] == 1 && i != j){ graph[i].push_back(j);  graph[j].push_back(i);}}}
-        for(int i=0; i<n; i++){if(!vis[i]){ dfscheck(i,vis,graph); ans++;}} return ans; }};
+        int n = nums.size(); int m = nums[0].size();
+        vector<int> graph[n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(i != j && nums[i][j] == 1){
+                    graph[i].push_back(j);
+                    graph[j].push_back(i);
+                }
+            }
+        }
+        int ans = 0;
+        vector<int> vis(n, 0);
+        for(int i=0; i<n; i++){
+            if(!vis[i]){
+                dfscheck(i,vis,graph); ans++;
+            }
+        }
+        return ans;
+    }
+};
