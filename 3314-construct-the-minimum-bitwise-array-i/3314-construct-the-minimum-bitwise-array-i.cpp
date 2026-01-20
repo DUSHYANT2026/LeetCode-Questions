c8a0d2358f5f960fd7d52104cca9bc138af2ff22
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2026-01-21 00:32:01 +0530

    Time: 0 ms (100%), Space: 25.1 MB (78.7%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -1,13 +1,36 @@
+// class Solution {
+// public:
+//     vector<int> minBitwiseArray(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> ans(n, -1);
+//         for(int i=0; i<n; i++){
+//             for(int j=1; j<nums[i]; j++){
+//                 if( (j | (j + 1)) == nums[i]){
+//                     ans[i] = j; break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> minBitwiseArray(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(n, -1);
-        for(int i=0; i<n; i++){
-            for(int j=1; j<nums[i]; j++){
-                if( (j | (j + 1)) == nums[i]){
-                    ans[i] = j;
-                    break;
+        vector<int> ans(n);
+        
+        for (int i = 0; i < n; i++) {
+            int x = nums[i];
+            if (x == 2) {
+                ans[i] = -1;
+            } else {
+                for (int bit = 0; bit < 31; bit++) {
+                    if (!((x >> (bit + 1)) & 1)) {
+                        ans[i] = x ^ (1 << bit);
+                        break;
+                    }
                 }
             }
         }
