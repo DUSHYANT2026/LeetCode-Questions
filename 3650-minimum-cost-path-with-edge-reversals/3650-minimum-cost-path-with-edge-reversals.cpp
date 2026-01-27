9d0534da41dec7dc4b6f3e831dc4a30004d1caaa
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2026-01-28 00:10:22 +0530

    Time: 838 ms (6.49%), Space: 445.9 MB (5.04%) - LeetHub

--- 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.cpp
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
     int minCost(int n, vector<vector<int>>& edges) {
-        vector<pair<int,int>> graph[n];
+        vector<vector<int>> graph[n];
 
         for(auto it : edges){
             graph[it[0]].push_back({it[1],it[2]});
             graph[it[1]].push_back({it[0],2*it[2]});
         }
         vector<int> dist(n,INT_MAX);
-        // vector<int> vis(n, 0);
+        vector<int> vis(n, 0);
 
         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
         pq.push({0,0});
         dist[0] = 0;
-        // vis[0] = 1;
+        vis[0] = 1;
 
         while(!pq.empty()){
             auto it = pq.top(); pq.pop();
             int dis = it[0]; int node = it[1];
 
-            // if(vis[node] != 1) vis[node] = 1;
+            if(vis[node] != 1) vis[node] = 1;
 
             if(node == n-1) return dis;
             for(auto i : graph[node]){
-                // if(!vis[i.first]){
-                    if(dist[i.first] > dis + i.second){
-                        dist[i.first] = dis + i.second;
-                        pq.push({dist[i.first],i.first});
+                if(!vis[i[0]]){
+                    if(dist[i[0]] > dis + i[1]){
+                        dist[i[0]] = dis + i[1];
+                        pq.push({dist[i[0]],i[0]});
                     }
-                // }
+                }
             }  
         }
         return -1;
     }
 };
+
+
