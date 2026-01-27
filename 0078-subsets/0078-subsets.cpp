8f5da3884d367f0242cd9028a19c06095e0d57ca
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2026-01-28 00:23:47 +0530

    Time: 2 ms (40.75%), Space: 10 MB (61.32%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,24 @@
 class Solution {
-public:
-    void dpcheck(int index, vector<vector<int>> &ans, vector<int> &nums, vector<int> temp){
-        if(index == nums.size()){
-            ans.push_back(temp);
-            return;
-        }
-        temp.push_back(nums[index]);
-        dpcheck(index+1,ans,nums,temp);
-        temp.pop_back();
-        dpcheck(index+1,ans,nums,temp);
+private:
+    void dpcheck(vector<vector<int>> &ans, vector<int> &temp,vector<int> &nums,
+    int index){
+        int n = nums.size();
+    if(index == n){
+        ans.push_back(temp);
+        return;
     }
+    if(index < n){
+        temp.push_back(nums[index]);
+        dpcheck(ans,temp,nums,index+1);
+        temp.pop_back();
+        dpcheck(ans,temp,nums,index+1);
+    }
+    }
+public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> temp;
-        dpcheck(0,ans,nums,temp);
+        dpcheck(ans,temp,nums,0);
         return ans;
     }
 };
