6a7b587fd6be6ea1b17ee31b5c6f22620d63d663
DUSHYANT <136222934+DUSHYANT2026@users.noreply.github.com>
2026-01-28 00:05:56 +0530

    Time: 460 ms (21.87%), Space: 329.7 MB (24.28%) - LeetHub

--- 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.cpp
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.cpp
@@ -8,27 +8,27 @@ public:
             graph[it[1]].push_back({it[0],2*it[2]});
         }
         vector<int> dist(n,INT_MAX);
-        vector<int> vis(n, 0);
+        // vector<int> vis(n, 0);
 
         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
         pq.push({0,0});
         dist[0] = 0;
-        vis[0] = 1;
+        // vis[0] = 1;
 
         while(!pq.empty()){
             auto it = pq.top(); pq.pop();
             int dis = it[0]; int node = it[1];
 
-            if(vis[node] != 1) vis[node] = 1;
+            // if(vis[node] != 1) vis[node] = 1;
 
             if(node == n-1) return dis;
             for(auto i : graph[node]){
-                if(!vis[i.first]){
+                // if(!vis[i.first]){
                     if(dist[i.first] > dis + i.second){
                         dist[i.first] = dis + i.second;
                         pq.push({dist[i.first],i.first});
                     }
-                }
+                // }
             }  
         }
         return -1;
