271c79413992db7d82881799dfbaecca44f2e5d8
DUSHYANT <dushyantmudgal1312@gmail.com>
2026-04-13 14:22:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.cpp
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.cpp
@@ -4,9 +4,7 @@ public:
         int ans = INT_MAX;
         int n = nums.size()-1;
         for(int i=0; i<=nums.size()/2; i++){
-            if(nums[i] == target){
-                ans = min(ans, abs(i-start));
-            }
+            if(nums[i] == target) ans = min(ans, abs(i-start));
             if(nums[n-i] == target) ans = min(ans, abs(abs(n-i) - start));
         }
         return ans;
