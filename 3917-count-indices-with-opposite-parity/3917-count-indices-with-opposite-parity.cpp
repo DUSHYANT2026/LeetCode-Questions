e0b6a1e0610dcd7476b83e629e837c3b44a3352b
DUSHYANT <dushyantmudgal1312@gmail.com>
2026-05-03 11:43:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.cpp
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.cpp
@@ -3,30 +3,27 @@ public:
     vector<int> countOppositeParity(vector<int>& nums) {
         vector<int> ans(nums.size(),0);
         int odd = 0; int even = 0;
-        for(auto it : nums){
-            if(it % 2 == 0) even++;
-            else odd++;
-        }
-        // for(int i=0; i<nums.size(); i++){
-        //     if(nums[i]%2 == 0){
-        //         ans[i] = odd;
-        //         odd--; 
-        //     }else{
-        //         ans[i] = even;
-        //         even--;
-        //     }
-        // }
-        for(int i=0; i<nums.size(); i++){
-            int odd = 0; int even = 0;
-            for(int j=i+1; j<nums.size(); j++){
-                if(nums[j]%2 == 0) even++;
-                else odd++;
-            }
+
+        for(int i=nums.size()-1; i >= 0; i--){
             if(nums[i]%2 == 0){
                 ans[i] = odd;
+                even++; 
+            }else{
+                ans[i] = even;
+                odd++;
             }
-            else ans[i] = even;
         }
+        // for(int i=0; i<nums.size(); i++){
+        //     int odd = 0; int even = 0;
+        //     for(int j=i+1; j<nums.size(); j++){
+        //         if(nums[j]%2 == 0) even++;
+        //         else odd++;
+        //     }
+        //     if(nums[i]%2 == 0){
+        //         ans[i] = odd;
+        //     }
+        //     else ans[i] = even;
+        // }
         return ans;
     }
 };
